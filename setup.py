bb4d21ba9568b4230683eae1335acfbde684ecc3
Dan Drown <ddrown@macrofab.com>
2023-05-02 18:32:21 +0000

    include pygerbv/ in package

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
-setuptools.setup(
+setup(
     name='pygerbv',
     version='1.0',
+    packages=find_packages(),
     description='Thin Gerbv wrapper in Python',
     python_requires='>=3.7',
     url='https://github.com/elephantech/PyGerbv',
